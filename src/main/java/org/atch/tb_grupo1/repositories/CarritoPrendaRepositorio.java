7d715113cc4943b9c206ca7bcb9ce04ef6e0e90d
andrestuestach <andrestuesta_22@outlook.com>
2024-09-20 19:14:24 -0500

    corregir error

--- src/main/java/org/atch/tb_grupo1/repositories/CarritoPrendaRepositorio.java
+++ src/main/java/org/atch/tb_grupo1/repositories/CarritoPrendaRepositorio.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface CarritoPrendaRepositorio extends JpaRepository<CarritoPrenda, Integer> {
+public interface CarritoPrendaRepositorio  {
 }
